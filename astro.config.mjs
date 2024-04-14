29e84a7ff65b8bd986a3e588e49cd5bbdb3e67ad
haruna <kharuna0404@gmail.com>
2024-04-14 20:31:26 +0000

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), react()]
+  integrations: [tailwind(), icon(), react()],
+  output: "server",
+  adapter: vercel()
 });
