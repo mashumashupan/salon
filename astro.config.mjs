b40e7ec48695a2322d4ac4dbdc6a02cc974cd806
haruna <kharuna0404@gmail.com>
2024-04-15 11:18:39 +0000

    コンテンツ画像２，３変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), react()],
-  output: "server",
+  output: "static",
   adapter: vercel(),
   vite: {
     ssr: {
